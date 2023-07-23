586fe284fb5000fc5452c6bb1f95d8fa7fc5d8dd
Unknown Beast <118689676+UnknownBeast10@users.noreply.github.com>
2023-07-23 14:51:31 -0300

    Add files via upload

---
+++ js/description.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Front-End Developer", "Back-End Developer", "Javascript Developer", "Node.js Developer", "HTML CSS Developer", "Lua Developer", "Java Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
